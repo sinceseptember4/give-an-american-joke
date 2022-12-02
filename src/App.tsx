9e63f5c323646de79a609438dfb9af662d63f14f
sinceseptember4 <sinceseptember4@gmail.com>
2022-12-02 12:42:10 +0900

    add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Jokes from './jokes.json';
-
+import Home from "./ component/home";
+import Api from "./ component/api";
+import Random from "./ component/random";
 function App() {
 const jokes =JSON.parse(JSON.stringify(Jokes.jokes));
 const jokesLength= jokes.length;
 let randnum = Math.floor( Math.random() * jokesLength );
 console.log(jokes[randnum].joke);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={`/`} element={<Home />} />
+        <Route path={`/api/`} element={<Api />} />
+        <Route path={`/api/ramdom`} element={<Random />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
