55b12b56767dd04d63ed465710db22089bfd56ff
sinceseptember4 <sinceseptember4@gmail.com>
2022-12-08 13:05:13 +0900

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Jokes from './component/jokes.json';
 import Home from "./component/home";
 import {Api} from "./component/api";
 import Random from "./component/random";
